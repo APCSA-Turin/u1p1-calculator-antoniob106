f1dc0e22761aed054570964fccca498131250768
antoniob106 <antoniobalbuena12345@gmail.com>
2025-09-29 16:51:26 +0000

    updating tipcalculator.java

--- src/main/java/com/example/TipCalculator.java
+++ src/main/java/com/example/TipCalculator.java
@@ -1,4 +1,6 @@
 package com.example;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class TipCalculator {
@@ -6,14 +8,24 @@ public class TipCalculator {
     public static String calculateTip(int people, int percent, double cost) { //You must use these  variable in your calculations
         //DO NOT DELETE ANY OF THE CODE BELOW      
 
+        DecimalFormat df = new DecimalFormat("#.00");
+        df.setRoundingMode(RoundingMode.HALF_UP);
+        double cost2 = df.format(cost);
+        double tipAmount = df.format(cost*(percent/100));Double.parseDouble
+        double totalCost = df.format(cost+tipAmount);
+        double perPersonCostBefore = df.format(cost/people);
+        double tipPerPerson = df.format(tipAmount/people);
+        double perPersonCostAfter = df.format(totalCost/people);
+
+
         String result = "-------------------------------\n" +
-                       "Total bill before tip: $" + "" + "\n" +
-                       "Total percentage: " + "" + "%\n" +
-                       "Total tip: $" + "" + "\n" +
-                       "Total Bill with tip: $" + "" + "\n" +
-                       "Per person cost before tip: $" + "" + "\n" +
-                       "Tip per person: $" + "" + "\n" +
-                       "Total cost per person: $" + "" + "\n" +
+                       "Total bill before tip: $" + cost2 + "\n" +
+                       "Total percentage: " + percent + "%\n" +
+                       "Total tip: $" + tipAmount + "\n" +
+                       "Total Bill with tip: $" + totalCost + "\n" +
+                       "Per person cost before tip: $" + perPersonCostBefore + "\n" +
+                       "Tip per person: $" + tipPerPerson + "\n" +
+                       "Total cost per person: $" + perPersonCostAfter + "\n" +
                        "-------------------------------\n";
 
         return result;
@@ -48,8 +60,8 @@ public class TipCalculator {
         int people=10; 
         int percent=8;
         double cost=10.5;              
-        //System.out.println(calculateTip(people,percent,cost));
-        System.out.println(extraCredit(people, percent, cost));
+        System.out.println(calculateTip(people,percent,cost));
+        //System.out.println(extraCredit(people, percent, cost));
     }
 }
         
